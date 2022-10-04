6c0aef37691be0920fb6dc243ba2a699bb9956db
Usmaarn <usmaarn@gmail.com>
2022-10-04 15:49:59 +0100

    new commit

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="overflow-x-auto relative">
-    <h1 class="md:text-xl capitalize p-5">
-        <b class="text-2xl">{{ items.length }}</b> 
-        users registered so far
+  <h1 class="md:text-xl capitalize p-5">
+      <b class="text-2xl">{{ items.length }}</b>
+      users registered so far
     </h1>
+  <div class="overflow-x-auto relative">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       <thead
         class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
@@ -18,11 +18,11 @@
       </thead>
       <tbody>
         <tr
-          v-for="item,index in items"
+          v-for="(item, index) in items"
           :key="item.id"
           class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
         >
-        <th
+          <th
             scope="row"
             class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
           >
@@ -35,7 +35,7 @@
             {{ item.name }}
           </th>
           <td class="py-4 px-6">{{ item.pvc }}</td>
-          <td class="py-4 px-6">0{{ item.tel }}</td>
+          <td class="py-4 px-6">{{ item.tel }}</td>
           <td class="py-4 px-6">{{ item.unit }}</td>
         </tr>
       </tbody>
@@ -44,21 +44,22 @@
 </template>
 
 <script>
-import { getUsers } from '../helper/firebase';
-
-
+import { getUsers } from "../helper/firebase";
 
 export default {
-  async mounted() {;
+  async mounted() {
     let users = await getUsers();
     this.items = users;
   },
-  data(){
-    return {items: []}
-  }
+  data() {
+    return { items: [] };
+  },
 };
 </script>
 
 <style scoped>
-    th, td{@apply whitespace-nowrap}
+th,
+td {
+  @apply whitespace-nowrap;
+}
 </style>
