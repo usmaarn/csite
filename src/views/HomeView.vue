5bafb72ecd833089d222d7343ddf943c83120a83
usman <usmaarn@gmail.com>
2022-12-13 00:53:10 +0100

    updated homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,28 +6,50 @@ const auth = useAuthStore();
 </script>
 
 <template>
+
   <section class="wrapper">
-    <Navbar />
-    <main>
-      <h1>We Welcome you to APC Progressive Movement</h1>
-      <RouterLink to="/form">
-        <a className="px-10 py-5 rounded bg-green-400 font-bold">
-          Generate your Poster
-        </a>
-      </RouterLink>
-      <RouterLink v-if="auth?.user" to="/data">
-        <a className="px-10 py-5 rounded bg-green-400 font-bold"> View Data </a>
-      </RouterLink>
+
+    <nav class="p-5 h-[80px] md:h-[100px] md:px-24 flex items-center bg-black/30 justify-between">
+      <a href="#" class="flex items-center gap-2">
+        <div class="p-1 bg-white w-14 h-14 rounded-full overflow-hidden">
+          <img src="/apc.png" alt="" class="w-full h-full rounded-full" />
+        </div>
+        <span class="text-2xl font-bold hidden md:inline">APC Campaign</span>
+      </a>
+
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="w-6 h-6 scale-[200%]">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+      </svg>
+    </nav>
+
+    <main class="p-5">
+      <div class="flex flex-col justify-end md:w-3/5 ml-auto py-10 space-y-5 md:space-y-10 md:justify-center h-full">
+          <h1 class="text-4xl text-center md:text-left md:text-8xl uppercase text-green-500 md:text-black font-black">Muhammad Sanni Dattijo</h1>
+          <h3 class="text-3xl text-center md:text-left md:text-6xl font-bold text-zinc-400">Senatorial Movement</h3>
+          <div class="flex flex-col md:flex-row gap-5">
+            <button class="px-10 py-5 rounded bg-green-500 font-bold shadow">
+              Verify your PVC status
+            </button>
+            <button
+              class="px-10 py-5 rounded border border-white/60 text-white/60 hover:bg-white/60 hover:text-black font-bold shadow">
+              Generate Flier
+            </button>
+          </div>
+      </div>
     </main>
   </section>
 </template>
 
 <style scoped lang="scss">
-main {
-  @apply flex-grow mt-16 max-w-[800px] mx-auto flex flex-col items-center gap-10;
+.wrapper {
 
-  h1 {
-    @apply text-center text-5xl md:text-7xl font-black text-white leading-[1.2];
+  main {
+    background: linear-gradient(rgba(0, 0, 0, 0.3), black), url("/dattijo.jpeg");
+    background-size: contain;
+    background-position: left top;
+    background-repeat: no-repeat;
+    height: calc(100vh - 80px);
   }
 }
 </style>
